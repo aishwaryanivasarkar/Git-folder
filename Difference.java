cd9b73c456d88249724f342d8269733a54551330
Aishwarya Nivasarkar <aishwarya.nivasarkar@gmail.com>
2021-08-02 20:27:55 +0530

    Difference file added

---
+++ Difference.java
@@ -0,0 +1,10 @@
+public class sum 
+{
+     public static void main(String[]) args
+     {
+         System.out.println("Hello World");
+         int a= 30;
+         int b= 20;
+         System.out.println("Difference is = "+a-b);
+     }
+}
