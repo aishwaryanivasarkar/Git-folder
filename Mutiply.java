de32a9703d3ece33ebf17ad3ec656dc535129db2
Aishwarya Nivasarkar <aishwarya.nivasarkar@gmail.com>
2021-08-02 20:15:57 +0530

    Mutiplication file added

---
+++ Mutiply.java
@@ -0,0 +1,10 @@
+public class sum 
+{
+     public static void main(String[]) args
+     {
+         System.out.println("Hello World");
+         int a= 10;
+         int b= 20;
+         System.out.println("Multiply is = "+a*b);
+     }
+}
